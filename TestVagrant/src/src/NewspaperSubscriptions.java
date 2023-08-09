a838ffcd6daa1629b58320d30ace3f5656788cc4
Anuj Swai <89419371+AnujSwai@users.noreply.github.com>
2023-08-10 00:18:49 +0530

    Add files via upload

--- TestVagrant/src/src/NewspaperSubscriptions.java
+++ TestVagrant/src/src/NewspaperSubscriptions.java
@@ -1,77 +1,83 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class NewspaperSubscriptions {
 
-    public static void main(String[] args) {
-        // Define the newspaper data
-        Map<String, double[]> newspapers = new HashMap<>();
-        newspapers.put("TOI", new double[]{3, 3, 3, 3, 3, 5, 6});
-        newspapers.put("Hindu", new double[]{2.5, 2.5, 2.5, 2.5, 2.5, 4, 4});
-        newspapers.put("ET", new double[]{4, 4, 4, 4, 4, 4, 10});
-        newspapers.put("BM", new double[]{1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5});
-        newspapers.put("HT", new double[]{2, 2, 2, 2, 2, 4, 4});
-        
-        // Input the user's weekly budget
-        double budget = getInput("Enter your weekly budget: ");
+	public static List<List<String>> findCombinationsWithinBudget(Map<String, Double> weeklyCosts, Double budget) {
+		  Map<Double, List<String>> dp = new HashMap<>();
+		  dp.put(0.0, new ArrayList<String>());
 
-        // Generate combinations of newspapers within the budget
-        List<List<String>> validCombinations = new ArrayList<>();
-        for (int comboLength = 1; comboLength <= newspapers.size(); comboLength++) {
-            generateCombinations(new ArrayList<>(newspapers.keySet()), comboLength, validCombinations);
-        }
+		  for (Map.Entry<String, Double> entry : weeklyCosts.entrySet()) {
+		   String key = entry.getKey();
+		   double cost = entry.getValue();
+		   Map<Double, List<String>> newCombinations = new HashMap<>();
 
-        // Display valid combinations within the budget
-        for (List<String> combo : validCombinations) {
-            double totalCost = getTotalCost(combo, newspapers);
-            if (totalCost <= budget) {
-                System.out.println(combo);  //+ ": Total Cost - " + totalCost);
-            }
-        }
-    }
+		   for (Map.Entry<Double, List<String>> dpEntry : dp.entrySet()) {
+		    double currentBudget = dpEntry.getKey();
+		    List<String> combination = dpEntry.getValue();
+		    double newBudget = currentBudget + cost;
 
-    private static double getInput(String prompt) {
-        // Use a scanner or any other method to get user input
-        // For simplicity, we'll use a hardcoded value here
-        return 40;
-    }
+		    if (newBudget <= budget) {
+		     List<String> newCombination = new ArrayList<>(combination);
+		     newCombination.add(key);
+		     newCombinations.put(newBudget, newCombination);
+		    }
+		   }
 
-    private static void generateCombinations(List<String> items, int length, List<List<String>> result) {
-        generateCombinationsHelper(items, length, 0, new ArrayList<>(), result);
-    }
+		   dp.putAll(newCombinations);
+		  }
 
-    private static void generateCombinationsHelper(List<String> items, int length, int start, List<String> current,
-                                                   List<List<String>> result) {
-        if (current.size() == length) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-        for (int i = start; i < items.size(); i++) {
-            current.add(items.get(i));
-            generateCombinationsHelper(items, length, i + 1, current, result);
-            current.remove(current.size() - 1);
-        }
-    }
+		  List<List<String>> validCombinations = new ArrayList<>();
+		  for (List<String> combination : dp.values()) {
+		   if (combination.size() >= 2) {
+		    validCombinations.add(combination);
+		   }
+		  }
 
-    private static double getTotalCost(List<String> combo, Map<String, double[]> newspapers) {
-        double totalCost = 0;
-        for (String newspaper : combo) {
-            double[] prices = newspapers.get(newspaper);
-            totalCost += getMinPrice(prices);
-        }
-        return totalCost;
-    }
+		  return validCombinations;
+		 }
 
-    private static double getMinPrice(double[] prices) {
-        double minPrice = Double.MAX_VALUE;
-        for (double price : prices) {
-            minPrice = Math.min(minPrice, price);
-        }
-        return minPrice;
-    }
-}
+		 public static void main(String[] args) {
 
+		  //Adding all newspapers data
+		  Map<String, List<Double>> weeklyCosts = new HashMap<>();
+		  weeklyCosts.put("\"TOI\"", Arrays.asList(3.0, 3.0, 3.0, 3.0, 3.0, 5.0, 6.0));
+		  weeklyCosts.put("\"Hindu\"", Arrays.asList(2.5, 2.5, 2.5, 2.5, 2.5, 4.0, 4.0));
+		  weeklyCosts.put("\"ET\"", Arrays.asList(4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 10.0));
+		  weeklyCosts.put("\"BM\"", Arrays.asList(1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5));
+		  weeklyCosts.put("\"HT\"", Arrays.asList(2.0, 2.0, 2.0, 2.0, 2.0, 4.0, 4.0));
+
+		  //creating new HashMap and storing total count(Rs.) of all newspapers
+		  Map<String, Double> weeklySums = new HashMap<>();
+		  for (Map.Entry<String, List<Double>> entry : weeklyCosts.entrySet()) {
+		   double su = 0;
+		   for (Double x : entry.getValue()) {
+		    su += x;
+		   }
+		   weeklySums.put(entry.getKey(), su);
+		  }
+		   
+		  System.out.println("Enter weekly budget");
+		  try (Scanner scanner = new Scanner(System.in)) {
+			double weeklyBudget = scanner.nextDouble();
+			  
+			  
+			  
+
+			  List<List<String>> validCombinations = findCombinationsWithinBudget(weeklySums, weeklyBudget);
+
+			  System.out.println("Valid Combinations:");
+			  System.out.print(weeklyBudget+"=");
+			  for (List<String> combination : validCombinations) {
+			    String val =combination.toString().replace("[", "{").replace("]", "}");
+			    
+			    System.out.print(val +",");
+			  }
+		}}
+		 }
